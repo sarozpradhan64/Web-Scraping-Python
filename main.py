afc2bd31103cc260c5051179a76a274f2a7aac2b
Saroj Pradhan <sarozpradhan64@gmail.com>
2023-03-22 20:38:08 +0000

    FAST API and Next JS integration

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.kumarijob.com/"
-page = requests.get(URL)
+from fastapi import FastAPI
+app = FastAPI()
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(class_="job-list")
-# print(results.prettify())
-job_elements = results.find_all("li")
 
-for job_element in job_elements:
-  title_element = job_element.find("h5")
-  company_element = job_element.find("div", class_="text-wrap").find("a")
+@app.get("/")
+async def root():
   
-  job_title = title_element.text.strip() #strip() removed whitespaces
-  company_name = company_element.text.strip()
-  print(f"Job Title: {job_title}")
-  print(f"Company: {company_name}")
-  print("-------------------------")
-  print("\n")
+  URL = "https://www.kumarijob.com/"
+  page = requests.get(URL)
 
+  soup = BeautifulSoup(page.content, "html.parser")
+  results = soup.find(class_="job-list")
+  # print(results.prettify())
+  job_elements = results.find_all("li")
+  collection=[]
+  for job_element in job_elements:
+    title_element = job_element.find("h5")
+    company_element = job_element.find("div", class_="text-wrap").find("a")
+    
+    job_title = title_element.text.strip() #strip() removed whitespaces
+    company_name = company_element.text.strip()
+    detail = {'job_title':job_title, 'company_name':company_name}
+    collection.append(detail)
+    
+  return {"message": collection}
